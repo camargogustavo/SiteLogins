583f942a96b5bdbb36099d9624d957930e47175e
Camargão <103455889+camargogustavo@users.noreply.github.com>
2022-09-16 16:50:25 -0300

    Add files via upload

--- sitelogin/teladec.php
+++ sitelogin/teladec.php
@@ -5,24 +5,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <title>DDropdown</title>
 </head>
 
 <body>
     <nav class="dp-menu">
         <ul>
-            <li>    <a href="">Home</a>
-                <li><a href="">Cadastrar</a></li>
-                <li><a href="">Logins</a></li>
+            <li>
+                <a href="#">Home</a>
+                <ul>
+                    <li><a href="#">Cadastro</a></li>
+                    <li><a href="#">Logins</a></li>
+                </ul>
             </li>
-            <li>    <a href="#">Perfil</a>
-                <li><a href="">GitHub</a></li>
-                <li><a href="">Linkedin</a></li>
-                <li><a href="">Contato</a></li>
+            <li>
+                <a href="#">Perfil</a>
+                <ul>
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Linkedin</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+
             </li>
+        </ul>
     </nav>
-        
+
 
 </body>
 
